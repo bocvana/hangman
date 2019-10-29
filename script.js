1d8cebfb55af00dba4836ba21f9ec99db5e571ec
Bojana Djokic <43027122+bocvana@users.noreply.github.com>
2019-10-29 21:15:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -41,11 +41,11 @@
     ];
     var randomWord = words[ Math.floor(Math.random() * words.length)];
     var ul = document.getElementById("list");
+    var liElements = document.getElementsByClassName('dash');
     var inputLetter = document.getElementById("gameInput");
     var abeceda = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','u','w','x','y','z'];
     var abecedaContainer = document.getElementById("abeceda-container");
     var abecedaLetters = document.getElementsByClassName("abeceda-letter");
-    var liElements = document.getElementsByClassName('dash');
     var gameOverPopUp = document.getElementById("game-over-wraper");
     var gameOverScore = document.getElementById("game-over-score");
     var continueWraper = document.getElementById("continue-wraper");
@@ -53,6 +53,7 @@
     var spanScoreClass = document.getElementById("continue-score");
     var score = document.getElementById("score");
     var saved = document.getElementById("saved");
+    var inputForm = document.getElementById("input-form");
     var diamond = `<img src="img/diamond.png" alt=""/>`;
     var existingChar = [];
     var wrongChar = [];
@@ -62,6 +63,7 @@
     var points = 0;
     var savedScore = 0;
     var resetGameTimeout = 0;
+    var clearOverTimeout = 0;
 
     createLi();
     createAbeceda();
@@ -85,7 +87,8 @@
       var letterContainer = '';
       
       for (var letter = 0; letter < abeceda.length; letter++) {
-        var abecedaLetter = `<article class="abeceda-letter">${ abeceda[letter] }</article>`
+        var abecedaLetter =  `
+        <article class="abeceda-letter" onclick="clickLetter('${ abeceda[letter] }')">${ abeceda[letter] }</article>`
         letterContainer += abecedaLetter;
       }
         abecedaContainer.innerHTML = letterContainer;
@@ -94,6 +97,7 @@
     function gameOverFunc(counter) {
       gameOverPopUp.classList.add("popup");
       gameOverScore.innerHTML = `You lost ${ diamond } ${ counter }`;
+      clearTimeout(clearOverTimeout);
     }
 
     function reload() {
@@ -101,6 +105,7 @@
     }
 
     function resetGame(counter) {
+      inputForm.classList.remove("try-whole");
       continueWraper.classList.add("popup");
       spanScoreClass.innerHTML = `You win ${ diamond } ${ counter + 5 }`;
       clearTimeout(resetGameTimeout);
@@ -125,96 +130,8 @@
       continueWraper.classList.remove("popup");
     }
 
-    function hangmanGame() {
-      var existCount = 0;
-      var wrongCharCounter = 0;
-      var ifCharExist = false;
-
-      if (inputLetter.value == inputLetter.value.toUpperCase()) {
-        inputLetter.value = inputLetter.value.toLowerCase();
-      }
-
-      for (var c = 0; c < existingChar.length; c++) {
-        if (inputLetter.value === existingChar[c]) {
-          existCount++;
-        }
-      }
-
-      for (var b = 0; b < wrongChar.length; b++) {
-        if (inputLetter.value === wrongChar[b]) {
-          wrongCharCounter++;
-        }
-      }
-
-      for (var concats = 0; concats < concatArray.length; concats++) {
-          
-        if (inputLetter.value === concatArray[concats]) {
-
-          for (var d = 0; d < abecedaLetters.length; d++) {
-            abecedaLetters[d].classList.remove("scale-grow");
-
-            if (inputLetter.value === abecedaLetters[d].innerHTML) {
-              let letter = abecedaLetters[d];
-              abecedaLetters[d].classList.add("scale-grow");
-              var timeout = setTimeout(function(){
-                  letter.classList.remove("scale-grow");
-                }, 500);
-            } 
-          }
-        }
-      }
-      
-      if (randomWord.length === inputLetter.value.length && randomWord === inputLetter.value) {
-        for (var x = 0; x < inputLetter.value.length; x++) {
-          liElements[x].innerText = inputLetter.value[x];
-        }
-        pointCounter = randomWord.length;
-        ifCharExist = true;
-
-      } else if (inputLetter.value.length > 1 && !ifCharExist) {
-        counter--;
-        if (counter > 0) {
-          score.innerText = counter;
-        } else {
-          score.innerText = counter;
-          gameOverFunc(counter);
-        }
-      }
-    
-      for (var a = 0; a < randomWord.length; a++) { 
-      
-        if (randomWord[a] === inputLetter.value) {
-          liElements[a].innerText = inputLetter.value;
-          ifCharExist = true;
-
-          if (existCount === 0) {
-            pointCounter++;
-          }
-        } 
-      }
-
-      if (ifCharExist && existCount === 0 && inputLetter.value.length === 1) {
-        existingChar.push(inputLetter.value);
-      }
-
-      if (!ifCharExist && inputLetter.value.length === 1 && wrongCharCounter === 0) { 
-        wrongChar.push(inputLetter.value);
-        counter--;
-        if (counter > 0) {
-            score.innerText = counter;
-        } else {
-            score.innerText = counter;
-            gameOverFunc(counter);
-        }
-      }
-
-      for (var abc = 0; abc < abecedaLetters.length; abc++) {
-        if (inputLetter.value === abecedaLetters[abc].innerHTML) {
-          abecedaLetters[abc].classList.add("exist");
-        }
-      }
-      
-      if (pointCounter === randomWord.replace(/ /g, '').length || randomWord.length === pointCounter) {
+    function countScore() {
+      if (pointCounter === randomWord.replace(/ /g, '').length) {
         points = 10;
         savedScore += points;
         saved.innerHTML = savedScore;
@@ -222,8 +139,135 @@
           resetGame(counter);
         },700);
       } 
+    }
+ 
+    function writeWord() {
 
+      for (let z = 0; z < randomWord.length; z++) {
+        liElements[z].innerText = randomWord[z];
+        liElements[z].classList.add("write-word");
+      }
+    }
+    
+    function checkInput() {
+      ifCharExist = false;
+    
+      if (randomWord.length === inputLetter.value.length && randomWord === inputLetter.value) {
+
+        for (var x = 0; x < inputLetter.value.length; x++) {
+          liElements[x].innerText = inputLetter.value[x];
+        }
+
+        pointCounter = randomWord.replace(/ /g, '').length;
+        ifCharExist = true; 
+        countScore();
+      } else if (inputLetter.value.length > 1 && !ifCharExist) {
+          counter--;
+          if (counter > 0) {
+            score.innerText = counter;
+          } else {
+            score.innerText = counter;
+           writeWord();
+           clearOverTimeout= setTimeout(function() {
+              gameOverFunc(counter);
+            }, 2500);
+          }
+      }
+      inputLetter.value = "";
+      
+      if (counter === 0) {
+        window.checkInput = function() {return false};
+        window.clickLetter = function() {return false};
+      }
+    } 
+
+    function clickLetter(letters) {
+      var existCount = 0;
+      var wrongCharCounter = 0;
+      ifCharExist = false;
+
+      for (var c = 0; c < existingChar.length; c++) {
+        if (letters === existingChar[c]) {
+          existCount++;
+        }
+      }
+
+      for (var b = 0; b < wrongChar.length; b++) {
+        if (letters === wrongChar[b]) {
+          wrongCharCounter++;
+        }
+      }
+
+      for (var concats = 0; concats < concatArray.length; concats++) {
+          
+        if (letters === concatArray[concats]) {
+
+          for (var d = 0; d < abecedaLetters.length; d++) {
+            abecedaLetters[d].classList.remove("scale-grow");
+
+            if (letters === abecedaLetters[d].innerHTML) {
+              let letter = abecedaLetters[d];
+              abecedaLetters[d].classList.add("scale-grow");
+              var timeout = setTimeout(function(){
+                letter.classList.remove("scale-grow");
+              }, 500);
+            } 
+          }
+        }
+      }
+
+      for (var i = 0; i < randomWord.length; i++) {
+
+        if (letters === randomWord[i]) {
+          liElements[i].innerText = letters;
+          ifCharExist = true;
+
+          if (existCount === 0) {
+            pointCounter++;
+          }
+        }
+      }
+
+      if (ifCharExist && existCount === 0) {
+        existingChar.push(letters);
+      }
+
+      if (!ifCharExist && wrongCharCounter === 0) { 
+        wrongChar.push(letters);
+        counter--;
+        if (counter > 0) {
+            score.innerText = counter;
+        } else {
+            score.innerText = counter;
+            writeWord();
+            clearOverTimeout = setTimeout(function() {
+              gameOverFunc(counter);
+            }, 2500);
+        }
+      }
+
+      for (var abc = 0; abc < abecedaLetters.length; abc++) {
+        var resizeLetter = abecedaLetters[abc].innerText.toLowerCase();
+      
+        if (letters === resizeLetter) {
+          abecedaLetters[abc].classList.add("exist");
+        }
+      }
+
+      if ((randomWord.replace(/ /g, '').length - 3) <= pointCounter && randomWord.length > 7) {
+        inputForm.classList.add('try-whole');
+      } else if ((randomWord.replace(/ /g, '').length - 2) <= pointCounter) {
+        inputForm.classList.add('try-whole');
+      }
+
+      countScore();
       concatArray = existingChar.concat(wrongChar);
 
-      inputLetter.value = "";
+      if (counter === 0) {
+        window.clickLetter = function() {return false};
+        window.checkInput = function() {return false};
+      }
     };
+
+
+    
